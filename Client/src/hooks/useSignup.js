f39f33b33ee0f5f616c094c87a1926c620b4bf28
mhilgner <119048141+jaKusmara@users.noreply.github.com>
2023-11-25 16:32:32 +0100

    updated auth

--- Client/src/hooks/useSignup.js
+++ Client/src/hooks/useSignup.js
@@ -1,33 +1,43 @@
-import axios from 'axios';
-import { useAuthContext } from './useAuthContext';
-import { useState } from 'react';
+import axios from "axios";
+import { useAuthContext } from "./useAuthContext";
+import { useState } from "react";
 
 export const useSignup = () => {
   const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(false); 
+  const [isLoading, setIsLoading] = useState(false);
   const { dispatch } = useAuthContext();
 
-  const signup = async (firstname, surname, email, username, password, reTypePassword) => {
-    setIsLoading(true); 
+  const signup = async (
+    firstname,
+    surname,
+    email,
+    personalID,
+    phone,
+    password,
+    reTypePass
+  ) => {
+    setIsLoading(true);
 
     try {
-      const response = await axios.post('http://localhost:5000/api/user/signup', {firstname, surname, email, username, password, reTypePassword});
+      const response = await axios.post(
+        "http://localhost:5000/api/user/signup",
+        { firstname, surname, email, personalID, phone, password, reTypePass }
+      );
 
       if (response.status === 200) {
-        localStorage.setItem('user', JSON.stringify(response.data));
+        localStorage.setItem("user", JSON.stringify(response.data));
 
-        
         setIsLoading(false);
-        dispatch({ type: 'LOGIN', payload: response.data });
-        console.log('Registered successfully:', response.data);
+        dispatch({ type: "LOGIN", payload: response.data });
+        console.log("Registered successfully:", response.data);
       }
     } catch (error) {
       setIsLoading(false);
       setError(error.response.data.error);
-      
-      console.error('Registration failed:', error.response.data.error);
+
+      console.error("Registration failed:", error.response.data.error);
     }
   };
 
-  return { signup, error, isLoading }; 
+  return { signup, error, isLoading };
 };
