70423341c27ed0122036ba175129fb2db306c8c0
Vojtko Máté <fedohun@gmail.com>
2023-11-25 23:45:33 +0100

    Users1

---
+++ Client/src/components/User.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const User = ({}) => (
+  <div className='grid grid-cols-5 w-full h-full px-2 place-items-center'>
+    <h1 className='mx-2'>Janko</h1>
+    <h1 className='mx-2'>Hrasko</h1>
+    <h1 className='mx-2'>ASDASD@asdasd.asd</h1>
+    <h1 className='mx-2'>69696969</h1>
+    <h1 className='mx-2'>6969696969</h1>
+  </div>
+);
+
+export default User;
--- Client/src/pages/Users.jsx
+++ Client/src/pages/Users.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import AdminPanel from './AdminPanel'
+import User from '../components/User'
 
 export default function Users() {
   return (
     <div className="flex flex-col-2">
         <AdminPanel/>
-        <div className="px-8">
-            Users
+        <div className="flex flex-col py-2 m-2 h-full w-full bg-gray-300 rounded-3xl">
+          <div className='grid grid-cols-5 w-full h-full px-2 place-items-center font-bold text-xl'>
+            <h1>First Name:</h1>
+            <h1>Surname:</h1>
+            <h1>Email:</h1>
+            <h1>ID:</h1>
+            <h1>Phone:</h1>
+          </div>
+          <User />
         </div>
     </div>
   )
