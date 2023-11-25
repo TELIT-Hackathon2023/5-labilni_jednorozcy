3e636901e1b249a4c00ad82131fd68c5944aae5e
Vojtko Máté <fedohun@gmail.com>
2023-11-25 16:10:56 +0100

    reg1

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,6 +3,7 @@ import  Login from './pages/Login.jsx'
 import   Home from  './pages/Home.jsx'
 import Error from './pages/Error.jsx'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from './pages/Register.jsx'
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
 				<Route path="/" element={<Home/>}/>
 				<Route path="/login" element={<Login />}/>
 				<Route path="*" element={<Error />} />
+				<Route path="/register" element={<Register />} />
 			</Routes>
 
 		</BrowserRouter>
--- Client/src/pages/Register.jsx
+++ Client/src/pages/Register.jsx
@@ -1,90 +1,78 @@
 const Register = () => {
   return(
-    <div className="flex h-screen items-center justify-center bg-magenta">
-			<form className="flex flex-col w-80 h-96 justify-around rounded-lg bg-white p-6">
-				<div className="flex flex-col">
-					<label htmlFor="firstname" className="pb-2">
-						First Name
-					</label>
-					<input
-						defaultValue="firstname"
-						id="firstname"
-						className="border-2 p-1 border-gray-800 rounded-md"
-					></input>
-				</div>
-				<div className="flex flex-col">
-					<label htmlFor="surname" className="pb-2">
-						Surname
-					</label>
-					<input
-						defaultValue="surname"
-						id="surname"
-						className="border-2 p-1 border-gray-800 rounded-md"
-					></input>
-				</div>
-        <div className="flex flex-col">
-					<label htmlFor="email" className="pb-2">
+    <div className="flex flex-col h-screen items-center justify-center bg-magenta">
+			<h1 className="text-4xl font-bold text-white capitalize dark:text-white p-2">Register</h1>
+			<form className="max-w-4xl p-6 mx-auto bg-white rounded-md shadow-md">
+        <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+          <div className="flex flex-col">
+            <label htmlFor="firstname" className="pb-1">
+              First Name
+            </label>
+            <input
+              id="firstname"
+              className="border-2 p-1 border-gray-800 rounded-md"
+            ></input>
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="surname" className="pb-1">
+              Surname
+            </label>
+            <input
+              id="surname"
+              className="border-2 p-1 border-gray-800 rounded-md"
+            ></input>
+          </div>
+          <div className="flex flex-col">
+					<label htmlFor="email" className="pb-1">
 						Email
 					</label>
 					<input
-						defaultValue="email"
 						id="email"
 						className="border-2 p-1 border-gray-800 rounded-md"
 					></input>
-				</div>
-        <div className="flex flex-col">
-					<label htmlFor="personalID" className="pb-2">
-						Personal ID
-					</label>
-					<input
-						defaultValue="personalID"
-						id="personalID"
-						className="border-2 p-1 border-gray-800 rounded-md"
-					></input>
-				</div>
-        <div className="flex flex-col">
-					<label htmlFor="lpn" className="pb-2">
-						License Plate Number
-					</label>
-					<input
-						defaultValue="lpn"
-						id="lpn"
-						className="border-2 p-1 border-gray-800 rounded-md"
-					></input>
-				</div>
-        <div className="flex flex-col">
-					<label htmlFor="phone" className="pb-2">
-						Phone Number
-					</label>
-					<input
-						defaultValue="phone"
-						id="phone"
-						className="border-2 p-1 border-gray-800 rounded-md"
-					></input>
-				</div>
-        <div className="flex flex-col">
-					<label htmlFor="password" className="pb-2">
-						Password
-					</label>
-					<input
-						defaultValue="password"
-						id="password"
-						className="border-2 p-1 border-gray-800 rounded-md"
-					></input>
-				</div>
-        <div className="flex flex-col">
-					<label htmlFor="retypePass" className="pb-2">
-						Retype Password
-					</label>
-					<input
-						defaultValue="retypePass"
-						id="retypePass"
-						className="border-2 p-1 border-gray-800 rounded-md"
-					></input>
-				</div>
-				<button className="p-1 border-2 border-magenta rounded-md bg-magenta text-white hover:bg-white hover:text-black hover:font-bold">
-					Register
-				</button>
+					</div>
+					<div className="flex flex-col">
+						<label htmlFor="personalID" className="pb-1">
+							Personal ID
+						</label>
+						<input
+							id="personalID"
+							className="border-2 p-1 border-gray-800 rounded-md"
+						></input>
+					</div>
+					<div className="flex flex-col">
+						<label htmlFor="phone" className="pb-1">
+							Phone Number
+						</label>
+						<input
+							id="phone"
+							className="border-2 p-1 border-gray-800 rounded-md"
+						></input>
+					</div>
+					<div className="flex flex-col">
+						<label htmlFor="password" className="pb-1">
+							Password
+						</label>
+						<input
+							id="password"
+							className="border-2 p-1 border-gray-800 rounded-md"
+						></input>
+					</div>
+					<div className="flex flex-col">
+						<label htmlFor="retypePass" className="pb-1">
+							Retype Password
+						</label>
+						<input
+							id="retypePass"
+							className="border-2 p-1 border-gray-800 rounded-md"
+						></input>
+					</div>
+					<div className="flex justify-center items-center ">
+						<button className="py-1 px-8 m-0 border-2 border-magenta rounded-md bg-magenta text-white hover:bg-white hover:text-black hover:font-bold">
+						Register
+						</button>
+					</div>
+        </div>
 			</form>
 		</div>
   )
